a3651c0ffd213dbfbd0cc09b964854ba7b1449d9
rockrick1 <henriquecerquinho@gmail.com>
2024-08-19 22:30:33 -0300

    feat: add more sounds and lava scene

--- Scripts/Characters/AudioPlayDamage.gd
+++ Scripts/Characters/AudioPlayDamage.gd
@@ -1,11 +1,10 @@
 extends AudioStreamPlayer2D
 
 @onready var character_component = $"../CharacterComponent"
-@onready var audio_stream_player_2d = $"."
 
 
 func _ready():
 	character_component.damaged.connect(_on_signal)
 	
 func _on_signal(amount: int):
-	audio_stream_player_2d.play()
+	play()
